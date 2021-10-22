098724e9447b9cd44450ff2926074364de59cee2
Roderick <m.contactqc@gmail.com>
2021-10-22 19:41:48 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using Authorizations.Models;
+using Authorizations.Repositories;
+using Authorizations.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorizations.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult<dynamic> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "User or password invalid" });
+
+            var token = TokenService.CreateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous()
+        {
+            return "You are Anonymous";
+        }
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Authenticated - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("tester")]
+        [Authorize(Roles = "tester")]
+        public string Tester()
+        {
+            return "You are a Tester";
+        }
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Employee";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Manager";
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Repositories/UserRepositories.cs
@@ -0,0 +1,26 @@
+﻿using Authorizations.Models;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorizations.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "goku", Password = "goku", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "vejeta", Password = "vejeta", Role = "employee" });
+            users.Add(new User { Id = 3, Username = "kuririn", Password = "kuririn", Role = "tester" });
+            //users.Add(new User (1, "goku", "goku", "manager"));
+            //users.Add(new User(2, "vejeta", "vejeta", "employee"));
+            //users.Add(new User(3, "kuririn", "kuririn", "tester"));
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
